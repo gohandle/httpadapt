4cc691f9fe82d2ab690800ef130b7e890fd2bb28
advanderveer <advanderveer@gmail.com>
2020-11-13 13:51:02 +0100

    added two options

--- adapter.go
+++ adapter.go
@@ -5,11 +5,14 @@ import "net/http"
 // Adapter adapts http.Handler implementations.
 type Adapter struct {
 	h http.Handler
+
+	stripBasePath       string
+	customServerAddress string
 }
 
 // New inits an adapter
 func New(h http.Handler, opts ...Option) (a *Adapter) {
-	a = &Adapter{h}
+	a = &Adapter{h: h}
 	for _, opt := range opts {
 		opt(a)
 	}
--- option.go
+++ option.go
@@ -2,3 +2,20 @@ package httpadapt
 
 // Option configures the adapter
 type Option func(a *Adapter)
+
+// StripBasePath configures the adapter to strip the prefixing part of the
+// resulting url path
+func StripBasePath(base string) Option {
+	return func(a *Adapter) {
+		a.stripBasePath = base
+	}
+}
+
+// CustomHost configures the custom hostname for the request. If this option
+// is not set the framework reverts to `RequestContext.DomainName`. The value
+// for a custom host should include a protocol: http://my-custom.host.com
+func CustomHost(host string) Option {
+	return func(a *Adapter) {
+		a.customServerAddress = host
+	}
+}
