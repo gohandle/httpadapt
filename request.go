135c60566f1e58271492b33b2b769762f965ee22
advanderveer <advanderveer@gmail.com>
2020-11-13 13:59:48 +0100

    start of cleanup

--- request.go
+++ request.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"path"
 	"sort"
 	"strings"
 
@@ -14,30 +15,29 @@ import (
 
 // EventToRequest converts an API Gateway proxy event into an http.Request object.
 // Returns the populated request maintaining headers
-func (a *Adapter) EventToRequest(ev events.APIGatewayProxyRequest) (*http.Request, error) {
-	decodedBody := []byte(ev.Body)
+func (a *Adapter) EventToRequest(ev events.APIGatewayProxyRequest) (req *http.Request, err error) {
+	body := []byte(ev.Body)
 	if ev.IsBase64Encoded {
-		base64Body, err := base64.StdEncoding.DecodeString(ev.Body)
+		body, err = base64.StdEncoding.DecodeString(ev.Body)
 		if err != nil {
 			return nil, fmt.Errorf("failed to decode base64 body: %w", err)
 		}
-		decodedBody = base64Body
 	}
 
-	path := ev.Path
-	if a.stripBasePath != "" {
-		if strings.HasPrefix(path, a.stripBasePath) {
-			path = strings.Replace(path, a.stripBasePath, "", 1)
-		}
+	p := ev.Path
+	if a.stripBasePath != "" && strings.HasPrefix(p, a.stripBasePath) {
+		p = strings.Replace(p, a.stripBasePath, "", 1)
 	}
-	if !strings.HasPrefix(path, "/") {
-		path = "/" + path
+
+	if !path.IsAbs(p) {
+		p = path.Join("/", p)
 	}
+
 	serverAddress := "https://" + ev.RequestContext.DomainName
 	if a.customServerAddress != "" {
 		serverAddress = a.customServerAddress
 	}
-	path = serverAddress + path
+	p = serverAddress + p
 
 	if len(ev.MultiValueQueryStringParameters) > 0 {
 		queryString := ""
@@ -56,7 +56,7 @@ func (a *Adapter) EventToRequest(ev events.APIGatewayProxyRequest) (*http.Reques
 				queryString += url.QueryEscape(q) + "=" + url.QueryEscape(v)
 			}
 		}
-		path += "?" + queryString
+		p += "?" + queryString
 	} else if len(ev.QueryStringParameters) > 0 {
 
 		keys := make([]string, 0, len(ev.QueryStringParameters))
@@ -74,16 +74,13 @@ func (a *Adapter) EventToRequest(ev events.APIGatewayProxyRequest) (*http.Reques
 			}
 			queryString += url.QueryEscape(q) + "=" + url.QueryEscape(ev.QueryStringParameters[q])
 		}
-		path += "?" + queryString
+		p += "?" + queryString
 	}
 
-	req, err := http.NewRequest(
-		strings.ToUpper(ev.HTTPMethod),
-		path,
-		bytes.NewReader(decodedBody),
-	)
-
-	if err != nil {
+	if req, err = http.NewRequest(
+		strings.ToUpper(ev.HTTPMethod), p,
+		bytes.NewReader(body),
+	); err != nil {
 		return nil, fmt.Errorf("failed to create request: %w", err)
 	}
 
